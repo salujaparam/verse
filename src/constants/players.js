b4300b29aaef30878326ae4e2b7cf0d11fe873f4
salujaparam <salujaparam@gmail.com>
2019-09-10 10:56:14 +0530

    added pics

--- src/constants/players.js
+++ src/constants/players.js
@@ -1,44 +1,65 @@
 import marco from '../images/marco.jpg'
 import shado from '../images/hrishekesh.jpg'
-import kbr from '../images/aakash.jpeg'
+import kbr from '../images/kbr.jpeg'
 import kunal from '../images/kunal.jpeg'
-import killer from '../images/bhargav.jpeg'
+import killer from '../images/killer.jpeg'
 import satyaki from '../images/satyaki.jpeg'
+import param from '../images/param.jpg'
+import monk from '../images/monk.jpeg'
+import zeus from '../images/zeus.jpeg'
 
 export default [
     {
         image: marco,
-        name: "Shekhar",
+        name: "Shekhar (marco)",
         title: "Inspiration to others",
-        description: "Playing call of duty since 2010 and now a waiter in ITC."
+        description: "Playing call of duty since 2010 and currently working in ITC."
     },
     {
         image: shado,
-        name: "Hrishikesh",
+        name: "Hrishikesh (shado)",
         title: "Captain of the team",
         description: "Playing call of duty since 2012 and is high on weed most of the time."
     },
     {
-        image: satyaki,
-        name: "Satyaki",
-        title: "Bakchod of the group",
-        description: "Playing call of duty since 2008 and is a graphic engineer at the moment."
+        image: param,
+        name: "Param (kili)",
+        title: "Guide/Mentor",
+        description: "Playing call of duty since 2017 and helping others to play well."
+    },
+    {
+        image: monk,
+        name: "deepak (monk)",
+        title: "Rager of the team",
+        description: "Playing call of duty since 2013 and currently working for ISIS."
     },
     {
         image: kbr,
-        name: "Aakash ",
+        name: "Aakash (kbr)",
         title: "Pro in every game",
         description: "Playing call of duty and  since 2017 and is an elite player in pubg pc lite."
     },
     {
+        image: zeus,
+        name: "Gandu (zeus)",
+        title: "Always cries",
+        description: "Asking cover from smg's while doing ak since 2008."
+    },
+    {
+        image: satyaki,
+        name: "Satyaki (blkzu)",
+        title: "Bakchod of the group",
+        description: "Playing call of duty since 2008 and is a graphic engineer at the moment."
+    },
+    {
         image: kunal,
-        name: "Kunal",
+        name: "Kunal (aloy)",
         title: "Good at smg",
         description: "Playing call of duty since 2015 and is hoping to become a good scoper."
     },
     {
         image: killer,
-        name: "Bhargav",
+        name: "Bhargav (killer)",
         title: "No words to describe him",
         description: "Good at heart but as dumb as a person can get."
     },
